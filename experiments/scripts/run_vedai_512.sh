6b1a69c287bd271d6a31905c9ba165ac4492ca06
Sommer, Lars Wilko <lars.sommer@iosb.fraunhofer.de>
2017-11-22 16:45:45 +0100

    updated scripts

--- experiments/scripts/run_vedai_512.sh
+++ experiments/scripts/run_vedai_512.sh
@@ -6,7 +6,7 @@ echo Logging output to "$LOG"
 
 cd ../../
 
-./tools/train_net.py --gpu 0 --weights data/imagenet_models/VGG16.v2.caffemodel --imdb VEDAI_512_Train --cfg experiments/cfgs/vedai_512.yml --solver models/vedai/VGG16_anchor_3_scale_conv_3_3/faster_rcnn_end2end/solver.prototxt --iters 60000
+./tools/train_net.py --gpu 0 --weights models/initialization/VGG_ILSVRC_16_layers.caffemodel --imdb VEDAI_512_Train --cfg experiments/cfgs/vedai_512.yml --solver models/vedai/VGG16_anchor_3_scale_conv_3_3/faster_rcnn_end2end/solver.prototxt --iters 60000
 
 ./tools/test_net.py --gpu 0 --def models/vedai/VGG16_anchor_3_scale_conv_3_3/faster_rcnn_end2end/test.prototxt --net output/vedai512/vgg16_faster_rcnn_iter_60000.caffemodel --cfg experiments/cfgs/vedai_512.yml --imdb VEDAI_512_Test
 
